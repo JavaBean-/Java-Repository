0c23d1b3a66be6338e98a1b6590445140a803a56
Barry Xu <barry.xu@ericsson.com>
2019-11-12 17:12:25 +0800

    clean

--- network/src/main/java/pers/learn/jdk/rmi/Test.java
+++ network/src/main/java/pers/learn/jdk/rmi/Test.java
@@ -1,5 +1,20 @@
-package pers.learn.jdk;
+package pers.learn.jdk.rmi;
 
 public class Test {
+
+    public static void main(String[] argv){
+
+        Tree t =new Tree();
+        Apple apple = (Apple) t;
+
+    }
+
+}
+
+class Apple extends Tree{
+
+}
+
+class Tree{
 
 }
