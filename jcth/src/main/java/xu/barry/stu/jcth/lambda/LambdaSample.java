a31e2da50e5349e37b1ef9aa6045497d3d5daed3
JavaBean- <s329746927@gmail.com>
2020-03-08 09:52:04 +0800

    Create LambdaSample.java

---
+++ jcth/src/main/java/xu/barry/stu/jcth/lambda/LambdaSample.java
@@ -0,0 +1,9 @@
+package xu.barry.stu.jcth.lambda;
+
+public class LambdaSample {
+
+	public static String[] planets = new String[] { "Mercury", "Venus", "Earth", "Mars", "Saturn", "Jupiter", "Uranus",
+			"Neptune" };
+
+	
+}
