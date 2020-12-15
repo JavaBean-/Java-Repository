8c65f76688047bc9c380d33ae26b3eb715bbb009
ezxuxzh <s329746927@163.com>
2020-12-15 17:24:23 +0800

    Self implement lock

---
+++ jcth/src/main/java/xu/barry/stu/jcth/juc/lock/SelfLock.java
@@ -0,0 +1,95 @@
+package xu.barry.stu.jcth.juc.lock;
+
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+
+public class SelfLock implements Lock {
+    // 静态内部类，自定义同步器
+    private static class Sync extends AbstractQueuedSynchronizer {
+        //private static final long serialVersionUID = -4387327721959839431L;
+
+        //TODO
+
+        // 返回一个Condition，每个condition都包含了一个condition队列
+        Condition newCondition() {
+            return new ConditionObject();
+        }
+
+
+        protected boolean tryRelease(int releases) {
+            int c = getState() - releases;
+            if (Thread.currentThread() != getExclusiveOwnerThread())
+                throw new IllegalMonitorStateException();
+            boolean free = false;
+            if (c == 0) {
+                free = true;
+                setExclusiveOwnerThread(null);
+            }
+            setState(c);
+            return free;
+        }
+
+        protected boolean tryAcquire(int acquires) {
+            final Thread current = Thread.currentThread();
+            int c = getState();
+            if (c == 0) {
+                if (!hasQueuedPredecessors() &&
+                        compareAndSetState(0, acquires)) {
+                    setExclusiveOwnerThread(current);
+                    return true;
+                }
+            }
+            else if (current == getExclusiveOwnerThread()) {
+                int nextc = c + acquires;
+                if (nextc < 0)
+                    throw new Error("Maximum lock count exceeded");
+                setState(nextc);
+                return true;
+            }
+            return false;
+        }
+    }
+
+    // 仅需要将操作代理到Sync上即可
+    private final Sync sync = new Sync();
+
+    public void lock() {
+        System.out.println(Thread.currentThread().getName()+" ready get lock");
+        sync.acquire(1);
+        System.out.println(Thread.currentThread().getName()+" already got lock");
+    }
+
+    public boolean tryLock() {
+        return false;
+    }
+
+    public void unlock() {
+        System.out.println(Thread.currentThread().getName()+" ready release lock");
+        sync.release(1);
+        System.out.println(Thread.currentThread().getName()+" already released lock");
+    }
+
+    public Condition newCondition() {
+        return sync.newCondition();
+    }
+
+    public boolean isLocked() {
+        return false;//TODO
+    }
+
+    public boolean hasQueuedThreads() {
+        return sync.hasQueuedThreads();
+    }
+
+    public void lockInterruptibly() throws InterruptedException {
+        sync.acquireInterruptibly(1);
+    }
+
+    public boolean tryLock(long timeout, TimeUnit unit) throws InterruptedException {
+        return sync.tryAcquireNanos(1, unit.toNanos(timeout));
+    }
+}
---
+++ jcth/src/main/java/xu/barry/stu/jcth/juc/lock/TestSelfLock.java
@@ -0,0 +1,43 @@
+package xu.barry.stu.jcth.juc.lock;
+
+import org.junit.Test;
+import xu.barry.stu.jcth.utils.model.SleepTools;
+
+import java.util.concurrent.locks.Lock;
+
+public class TestSelfLock {
+
+
+    public void test() {
+        final Lock lock = new SelfLock();
+        class Worker extends Thread {
+
+            public void run() {
+                lock.lock();
+                System.out.println(Thread.currentThread().getName());
+                try {
+                    SleepTools.second(1);
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
+        // 启动4个子线程
+        for (int i = 0; i < 4; i++) {
+            Worker w = new Worker();
+            //w.setDaemon(true);
+            w.start();
+        }
+        // 主线程每隔1秒换行
+        for (int i = 0; i < 10; i++) {
+            SleepTools.second(1);
+            //System.out.println();
+        }
+    }
+
+    @Test
+    public void main() {
+        TestSelfLock testMyLock = new TestSelfLock();
+        testMyLock.test();
+    }
+}
