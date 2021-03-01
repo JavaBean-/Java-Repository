5f36a5b1311a44fc1e3e211f13c89ac0ae99a211
ezxuxzh <s329746927@163.com>
2021-03-01 16:54:33 +0800

    Add describe for submit/execute method

--- jcth/src/main/java/xu/barry/stu/jcth/juc/executors/Executor.java
+++ jcth/src/main/java/xu/barry/stu/jcth/juc/executors/Executor.java
@@ -8,6 +8,15 @@ import xu.barry.stu.jcth.utils.model.SleepTools;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * cpu 密集型程序
+ * 线程数量 = 机器cpu核心数+1
+ * IO 密集型程序
+ * 线程数量 = 机器cpu核心数*2
+ *
+ * 如果 混合型程序
+ * 尽量分割任务为：cpu密集型 + IO密集型
+ */
 public class Executor {
 
     private static int i = 0;
@@ -17,10 +26,20 @@ public class Executor {
         ExecutorService executor = Executors.newSingleThreadExecutor();
         for (int n = 0 ; n < 10 ; n ++){
 
-            executor.submit( ()-> {
+            /**
+             *  Callable 入参接口
+             *  会吞掉exception
+             *  只有调用future可以获得结果
+             */
+            //executor.submit( ()-> {
 
-
-            //executor.execute(() -> {
+            /**
+             *  Runnable 入参接口
+             *  打印出exception
+             *  newSingleThreadExecutor线程池执行
+             *  遇到抛出异常，会重新创建新线程继续执行后续任务
+             */
+            executor.execute(() -> {
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException e) {
